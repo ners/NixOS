abd7a4b480c4a58a30145a3ed58c9f71866c558e
ners <ners@gmx.ch>
2022-05-13 12:19:17 +0200

    Fix nested autocmd

--- profiles/users/common/neovim/plugin/nvim-tree.lua
+++ profiles/users/common/neovim/plugin/nvim-tree.lua
@@ -5,7 +5,7 @@ vim.g.nvim_tree_show_icons = {
 	folder_arrows = 0,
 }
 
-require'nvim-tree'.setup {
+require 'nvim-tree'.setup {
 	open_on_setup = true,
 	open_on_setup_file = true,
 	open_on_tab = true,
@@ -17,5 +17,6 @@ require'nvim-tree'.setup {
 -- Automatically close the tab/vim when nvim-tree is the last window in the tab
 vim.api.nvim_create_autocmd('BufEnter', {
 	group = vim.api.nvim_create_augroup('CloseNvimTreeWhenLast', {}),
-	command = "++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+	command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+	nested = true,
 })
