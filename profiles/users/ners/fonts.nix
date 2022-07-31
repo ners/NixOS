07d7b54f808ac18e06708729edbcac3b247ff103
ners <ners@gmx.ch>
2022-07-31 11:42:48 +0200

    Add some fonts

--- profiles/users/ners/fonts.nix
+++ profiles/users/ners/fonts.nix
@@ -1,18 +1,32 @@
 { pkgs, lib, ... }:
 
 let
-  gula = pkgs.stdenv.mkDerivation rec {
-    name = "gula";
+  mkFont = { name, url, sha256 }: pkgs.stdenv.mkDerivation rec {
+    inherit name;
     nativeBuildInputs = [ pkgs.unzip ];
     src = pkgs.fetchurl {
+      inherit url sha256;
       name = "${name}.zip";
-      url = "https://dl.dafont.com/dl/?f=gula";
-      sha256 = "0phk9n4v8c36841cbjl98d18yqfryp09m1rcdaqad05xqkwjkjlh";
     };
     setSourceRoot = "sourceRoot=`pwd`";
     installPhase = ''
-      install -Dm644 -D "Gula FREE.otf" "$out/share/fonts/opentype/Gula.otf"
+      for f in *.ttf; do
+        install -Dm644 -D "$f" "$out/share/fonts/truetype/${name}/$f"
+      done
+      for f in *.otf; do
+        install -Dm644 -D "$f" "$out/share/fonts/opentype/${name}/$f"
+      done
     '';
+  };
+  oswald = mkFont {
+    name = "oswald";
+    url = "https://www.fontsquirrel.com/fonts/download/oswald";
+    sha256 = "sha256-+E/XVTPLKA6zKa+QZ/IrPp0RjLNEKf5WOO63oXGrROk=";
+  };
+  gula = mkFont {
+    name = "gula";
+    url = "https://dl.dafont.com/dl/?f=gula";
+    sha256 = "sha256-kMop+cS9gKawaiyHmsD12WGPQkOJysUCQWYwtIlNE14=";
   };
 in
 {
@@ -21,7 +35,14 @@ in
     agave
     charis-sil
     crimson
+    eb-garamond
     gula
+    hasklig
     material-design-icons
+    monoid
+    oswald
+    paratype-pt-mono
+    paratype-pt-sans
+    paratype-pt-serif
   ];
 }
