0c95933eba113e57ab6cc6dcb89e09c77fdef620
ners <ners@gmx.ch>
2022-12-31 19:21:42 +0100

    update Normandy

--- nixos/normandy/hardware-configuration.nix
+++ nixos/normandy/hardware-configuration.nix
@@ -4,43 +4,51 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/c393bb6d-f50f-4622-a994-385cfc829ca7";
-    fsType = "btrfs";
-    options = [ "subvol=root" ];
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/c393bb6d-f50f-4622-a994-385cfc829ca7";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
 
-  boot.initrd.luks.devices."cryptroot".device =
-    "/dev/disk/by-uuid/1b98c7b1-cf90-4daa-8941-f9735903aba5";
+  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/1b98c7b1-cf90-4daa-8941-f9735903aba5";
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/F0A4-ECA9";
-    fsType = "vfat";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/9E20-E12E";
+      fsType = "vfat";
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/c393bb6d-f50f-4622-a994-385cfc829ca7";
-    fsType = "btrfs";
-    options = [ "subvol=home" ];
-  };
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/c393bb6d-f50f-4622-a994-385cfc829ca7";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/c393bb6d-f50f-4622-a994-385cfc829ca7";
-    fsType = "btrfs";
-    options = [ "subvol=nix" ];
-  };
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/c393bb6d-f50f-4622-a994-385cfc829ca7";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
 
   swapDevices = [ ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno2.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
