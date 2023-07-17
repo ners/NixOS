b00b228c908ca8e90d1feda3a2f36a1897881ed0
rekk <rekk@gmx.de>
2023-07-17 09:28:31 +0200

    Add more system settings

--- profiles/users/rekk/system/default.nix
+++ profiles/users/rekk/system/default.nix
@@ -13,5 +13,19 @@
 
     system.keyboard.enableKeyMapping = true;
     system.keyboard.remapCapsLockToEscape = true;
+
+      system.activationScripts.extraActivation.text = ''
+        # ⌘ + M no longer minimises windows
+        defaults write -g NSUserKeyEquivalents -dict-add "Im Dock ablegen" "\0"
+
+        # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+        defaults write com.apple.finder QuitMenuItem -bool true
+
+        # Finder: show hidden files by default
+        defaults write com.apple.finder AppleShowAllFiles -bool true
+
+        # When performing a search, search the current folder by default
+        defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+        '';
   };
 }
